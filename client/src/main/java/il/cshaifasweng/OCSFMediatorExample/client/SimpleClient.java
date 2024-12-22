a0580200fb7c2a2d2008dd685901b6e78471ab69
Lama7amoud <lama.hamoud12345@gmail.com>
2024-12-22 23:27:14 +0200

    assignment finished with video

--- client/src/main/java/il/cshaifasweng/OCSFMediatorExample/client/SimpleClient.java
+++ client/src/main/java/il/cshaifasweng/OCSFMediatorExample/client/SimpleClient.java
@@ -1,34 +1,85 @@
 package il.cshaifasweng.OCSFMediatorExample.client;
 
-import org.greenrobot.eventbus.EventBus;
-
-import il.cshaifasweng.OCSFMediatorExample.client.ocsf.AbstractClient;
 import il.cshaifasweng.OCSFMediatorExample.entities.Warning;
+import org.greenrobot.eventbus.EventBus;
+import il.cshaifasweng.OCSFMediatorExample.client.ocsf.AbstractClient;
 
 public class SimpleClient extends AbstractClient {
-	
-	private static SimpleClient client = null;
 
-	private SimpleClient(String host, int port) {
+	private static SimpleClient client = null;
+	private String host ;
+	private int port ;
+	private String symbol ;
+
+	public SimpleClient(String host, int port) {
 		super(host, port);
+		this.host = host;
+		this.port = port;
+	}
+
+	public String getSymbol() {
+		return this.symbol;
+	}
+
+	public static SimpleClient getClient(String host, int port) {
+		if (client == null) {
+			client = new SimpleClient(host, port);
+		}
+		return client;
 	}
 
 	@Override
 	protected void handleMessageFromServer(Object msg) {
-		if (msg.getClass().equals(Warning.class)) {
+		if (msg instanceof Warning) {
+			// Handle warning message
 			EventBus.getDefault().post(new WarningEvent((Warning) msg));
+			return;
 		}
-		else{
-			String message = msg.toString();
-			System.out.println(message);
+		if(msg.toString().equals("competitorwin"))
+		{
+			EventBus.getDefault().post("competitorwin");
+			return;
+		}
+		if(msg.toString().equals("playAgain"))
+		{
+			EventBus.getDefault().post("playAgain");
+			return;
+		}
+		if(msg.toString().equals("TEEKO"))
+		{
+			EventBus.getDefault().post("TEEKO");
+			return;
+		}
+		if(msg.toString().equals("playerTurn"))
+		{
+			EventBus.getDefault().post("playerTurn");
+			return;
+		}
+		if(msg.toString().equals("competitorTurn"))
+		{
+			EventBus.getDefault().post("competitorTurn");
+			return;
+		}
+		if(msg.toString().equals("competitorLeft"))
+		{
+			EventBus.getDefault().post("competitorLeft");
+			return;
+		}
+
+		if (msg instanceof TurnInfo) {
+			TurnInfo data = (TurnInfo) msg;
+			EventBus.getDefault().post(data);
+		}else if(msg instanceof String) {
+			if(msg.toString().startsWith("Yess , you are player X"))
+					symbol = "X";
+				else{
+					symbol = "O";
+				}
 		}
 	}
-	
-	public static SimpleClient getClient() {
-		if (client == null) {
-			client = new SimpleClient("localhost", 3000);
-		}
-		return client;
-	}
+
+
+
+
 
 }
