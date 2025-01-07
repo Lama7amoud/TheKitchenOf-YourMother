73a380e7e1116191a2d15f97140dc13b06d35b7a
oraibm <bomchekaw4@gmail.com>
2025-01-07 15:39:07 +0200

    Client

---
+++ client/src/main/java/il/cshaifasweng/OCSFMediatorExample/client/Client.java
@@ -0,0 +1,52 @@
+
+package il.cshaifasweng.OCSFMediatorExample.client;
+
+import il.cshaifasweng.OCSFMediatorExample.entities.Warning;
+import org.greenrobot.eventbus.EventBus;
+import il.cshaifasweng.OCSFMediatorExample.client.ocsf.AbstractClient;
+
+public class Client extends AbstractClient {
+
+    private static Client client = null;
+    private String host;
+    private int port;
+    private String symbol;
+
+    public Client(String host, int port) {
+        super(host, port);
+        this.host = host;
+        this.port = port;
+    }
+
+    public String getSymbol() {
+        return this.symbol;
+    }
+
+    public static Client getClient(String host, int port) {
+        if (client == null) {
+            client = new Client(host, port);
+        }
+        return client;
+    }
+
+    @Override
+    protected void handleMessageFromServer(Object msg) {
+        if (msg instanceof Warning) {
+            // Handle warning message
+            EventBus.getDefault().post(new WarningEvent((Warning) msg));
+            return;
+        }
+        if (msg.toString().equals()) {
+            EventBus.getDefault().post();
+        }
+        if (msg.toString().equals()) {
+            EventBus.getDefault().post();
+        }
+        if (msg.toString().equals()) {
+            EventBus.getDefault().post();
+        }
+        if (msg.toString().equals()) {
+            EventBus.getDefault().post();
+        }
+    }
+}
